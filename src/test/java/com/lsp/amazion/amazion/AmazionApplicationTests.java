7e055ecd8dfc4548f5c729c6fef02f447ad8a8a6
Luis Perdigao <luis_s_p@hotmail.com>
2018-10-15 19:25:23 +0100

    [issue-1] spring initializr

---
+++ src/test/java/com/lsp/amazion/amazion/AmazionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lsp.amazion.amazion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmazionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
